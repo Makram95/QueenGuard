0cec19616d29b8c414538ba5407e885129caf05f
Makram95 <marc.kramer95@icloud.com>
2021-01-26 15:17:33 +0100

    Initial Commit of Empty Project

---
+++ QueenGuard/QueenGuard/QueenGuardApp.swift
@@ -0,0 +1,17 @@
+//
+//  QueenGuardApp.swift
+//  QueenGuard
+//
+//  Created by Marc on 26.01.21.
+//
+
+import SwiftUI
+
+@main
+struct QueenGuardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
